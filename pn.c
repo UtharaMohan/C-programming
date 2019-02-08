849490e5bf8181ca11f4310a43653fadf2725c32
UtharaMohan <47448985+UtharaMohan@users.noreply.github.com>
2019-02-08 16:57:47 +0530

    Create pn.c

---
+++ pn.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n==0)
+	printf("Zero\n");
+	else if(n>0)
+	printf("Postive\n");
+	else 
+	printf("Negative\n");
+	// your code goes here
+	return 0;
+}
