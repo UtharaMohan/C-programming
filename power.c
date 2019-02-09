e690712976fae24d6a5d37f1da7d59709bd9ac1e
UtharaMohan <47448985+UtharaMohan@users.noreply.github.com>
2019-02-09 16:51:23 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    int n,k,result;
+    scanf("%d",&n);
+    scanf("%d",&k);
+    result=pow(n,k);
+    printf("%d",result);
+    return 0;
+}
